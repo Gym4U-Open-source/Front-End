1ef8f190c84c1942f1cee9baf3565afe89c86dc7
ClinderSGT <93946818+ClinderSGT@users.noreply.github.com>
2022-09-29 11:17:08 -0500

    Update: posts services

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,1 +1,43 @@
+import { Injectable } from '@angular/core';
+import {HttpClient,HttpErrorResponse, HttpHeaders} from "@angular/common/http";
+import {Observable, throwError} from "rxjs";
+import {catchError, retry} from "rxjs/operators";
+import {Post} from "../components/model/post";
 
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+//posts endpoint
+
+  basePath = 'http://localhost:3000/posts'
+
+  httpOptions={
+    headers: new HttpHeaders({
+      'Content-Type':'application/json'
+    })
+  }
+
+  constructor(private http:HttpClient) { }
+
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // Default error handling
+      console.log(`An error occurred: ${error.error.message} `);
+    } else {
+      // Unsuccessful Response Error Code returned from Backend
+      console.error(
+        `Backend returned code ${error.status}, body was: ${error.error}`
+      );
+    }
+    // Return Observable with Error Message to Client
+    return throwError(() => new Error('Something happened with request, please try again later'));
+  }
+
+  // Get All Hotels
+  getAll():Observable<Post>{
+    return this.http.get<Post>(this.basePath,this.httpOptions)
+      .pipe(retry(2),catchError(this.handleError))
+  }
+
+}
