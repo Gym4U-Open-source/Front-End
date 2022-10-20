167fcbdd0b575e773b569bd01da98a2539aa0034
csgt <u201920785@upc.edu.pe>
2022-10-19 23:18:11 -0500

    Feat: Add post

---
+++ src/app/components/view-post-comments/view-post-comments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-post-comments',
+  templateUrl: './view-post-comments.component.html',
+  styleUrls: ['./view-post-comments.component.css']
+})
+export class ViewPostCommentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
